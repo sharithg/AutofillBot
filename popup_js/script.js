0ab44c6c4775f6546ef2dab2a92f88eec372e1b8
Sharith Godamanna <sharithg@Shariths-MBP.cable.rcn.com>
2020-09-07 11:38:23 -0400

    Form data stored at local storage

---
+++ popup_js/script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var btn = document.getElementById("submit_btn");
+  btn.addEventListener("click", function (event) {
+    event.stopPropagation();
+    event.preventDefault();
+    //first name
+    chrome.storage.sync.set(
+      { first_name: document.getElementById("firstName").value },
+      function () {
+        console.log(
+          "Value is set to " + document.getElementById("firstName").value
+        );
+      }
+    );
+    //last name
+    chrome.storage.sync.set(
+      { last_name: document.getElementById("lastName").value },
+      function () {
+        console.log(
+          "Value is set to " + document.getElementById("lastName").value
+        );
+      }
+    );
+    //email address
+    chrome.storage.sync.set(
+      { email_address: document.getElementById("emailAddress").value },
+      function () {
+        console.log(
+          "Value is set to " + document.getElementById("emailAddress").value
+        );
+      }
+    );
+    //phone number
+    chrome.storage.sync.set(
+      { phone_number: document.getElementById("phoneNumber").value },
+      function () {
+        console.log(
+          "Value is set to " + document.getElementById("phoneNumber").value
+        );
+      }
+    );
+    //zipcode
+    chrome.storage.sync.set(
+      { zipcode: document.getElementById("zipcode").value },
+      function () {
+        console.log(
+          "Value is set to " + document.getElementById("zipcode").value
+        );
+      }
+    );
+    //zipcode
+    chrome.storage.sync.set(
+      { city: document.getElementById("city").value },
+      function () {
+        console.log("Value is set to " + document.getElementById("city").value);
+      }
+    );
+  });
+});
